9b6cf38644610703d439ee20d6d77c866d6849bd
h.sb <jksg01019@gmail.com>
2025-06-08 19:13:00 +0900

    doing

--- data_process.sh
+++ data_process.sh
@@ -4,7 +4,7 @@
 NUPLAN_DATA_PATH="/mnt/nuplan/dataset/nuplan-v1.1/splits/trainval" # nuplan training data path (e.g., "/data/nuplan-v1.1/trainval")
 NUPLAN_MAP_PATH="/mnt/nuplan/dataset/maps" # nuplan map path (e.g., "/data/nuplan-v1.1/maps")
 
-TRAIN_SET_PATH="/mnt/nuplan/processed/" # preprocess training data
+TRAIN_SET_PATH="/mnt/nuplan/dataset/processed/" # preprocess training data
 ###################################
 
 python data_process.py \
--- torch_run.sh
+++ torch_run.sh
@@ -6,7 +6,7 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 RUN_PYTHON_PATH="/mnt/nuplan/miniforge/envs/diffusion_planner/bin/python" # python path (e.g., "/home/xxx/anaconda3/envs/diffusion_planner/bin/python")
 
 # Set training data path
-TRAIN_SET_PATH="/mnt/nuplan/processed/" # preprocess data using data_process.sh
+TRAIN_SET_PATH="/mnt/nuplan/dataset/processed/" # preprocess data using data_process.sh
 TRAIN_SET_LIST_PATH="/mnt/nuplan/projects/Diffusion-Planner/diffusion_planner_training.json"
 ###################################
 
