49ec79d7d36dcdf808813ed0acbd2215ed3ba7f0
h.sb <jksg01019@gmail.com>
2025-06-22 11:31:38 +0900

    xy, yaw error

--- train_predictor.py
+++ train_predictor.py
@@ -142,7 +142,7 @@ def get_args():
     parser.add_argument('--save_utd',
                         type=int,
                         help='save frequency',
-                        default=10)
+                        default=1)
     parser.add_argument('--batch_size',
                         type=int,
                         help='batch size (default: 2048)',
