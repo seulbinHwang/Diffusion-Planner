040349f5a14f314bf776bc10721a8f4873450411
h.sb <jksg01019@gmail.com>
2025-07-18 16:28:07 +0900

    doing...

--- diffusion_planner/utils/npc_data_augmentation.py
+++ diffusion_planner/utils/npc_data_augmentation.py
@@ -50,7 +50,8 @@ class NPCStatePerturbation:
              [0, 0, 2, 0, 0, 0],
              [1, T, T ** 2, T ** 3, T ** 4, T ** 5],
              [0, 1, 2 * T, 3 * T ** 2, 4 * T ** 3, 5 * T ** 4],
-             [0, 0, 2, 6 * T, 12 * T ** 2, 20 * T ** 3]]).inverse()  # (6, 6)
+             [0, 0, 2, 6 * T, 12 * T ** 2, 20 * T ** 3]],
+             device=device, dtype=torch.float32).inverse()  # (6, 6)
         T = REFINE_HORIZON + TIME_INTERVAL
 
         self.coeff_matrix = torch.linalg.inv(
