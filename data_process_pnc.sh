9e426df9a620c2fee5f5960ca5af72fa795f5c5d
h.sb <jksg01019@gmail.com>
2025-08-05 16:21:08 +0900

    doing...

---
+++ data_process_pnc.sh
@@ -0,0 +1,15 @@
+###################################
+# User Configuration Section
+###################################
+NUPLAN_DATA_PATH="/media/user/E/dataset/nuplan-v1.1/splits/trainval" # nuplan training data path (e.g., "/data/nuplan-v1.1/trainval")
+NUPLAN_MAP_PATH="/media/user/E/dataset/maps" # nuplan map path (e.g., "/data/nuplan-v1.1/maps")
+
+TRAIN_SET_PATH="/media/user/E/dataset/processed/" # preprocess training data
+###################################
+
+python data_process.py \
+--data_path $NUPLAN_DATA_PATH \
+--map_path $NUPLAN_MAP_PATH \
+--save_path $TRAIN_SET_PATH \
+--total_scenarios 1000000 \
+
