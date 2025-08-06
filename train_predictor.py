8fb7b7a960786e4cce83de309494db7dfc59c9fd
h.sb <jksg01019@gmail.com>
2025-08-06 16:05:10 +0900

    doing...

--- train_predictor.py
+++ train_predictor.py
@@ -145,7 +145,7 @@ def get_args():
     parser.add_argument('--save_utd',
                         type=int,
                         help='save frequency',
-                        default=10)
+                        default=20)
     parser.add_argument('--batch_size',
                         type=int,
                         help='batch size (default: 2048)',
