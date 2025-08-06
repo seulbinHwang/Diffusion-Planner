495069c6d14a69265122241b27e6c4314c583b88
h.sb <jksg01019@gmail.com>
2025-08-06 10:38:52 +0900

    doing...

---
+++ torch_run_ceph.sh
@@ -0,0 +1,31 @@
+echo "Start downloading diffusion_planner_training.json"
+nubescli download \
+    labs-mlops/ad/research/pnc/hsb/dataset/processed_json/diffusion_planner_training.json \
+    /mnt/nuplan/projects/Diffusion-Planner/diffusion_planner_training.json \
+    -j $(nproc) \
+    --no-progress
+echo "Finish downloading diffusion_planner_training.json"
+
+echo "Start downloading processed dataset"
+nubescli dir-download \
+    labs-mlops/ad/research/pnc/hsb/dataset/processed \
+    /mnt/nuplan/dataset/processed \
+    -j $(nproc) \
+    --no-progress
+echo "Finish downloading processed dataset"
+
+#export CUDA_VISIBLE_DEVICES=0,1,2,3 #,4,5,6,7
+
+###################################
+# User Configuration Section
+###################################
+RUN_PYTHON_PATH="/mnt/nuplan/miniforge/envs/diffusion_planner/bin/python" # python path (e.g., "/home/xxx/anaconda3/envs/diffusion_planner/bin/python")
+
+# Set training data path
+TRAIN_SET_PATH="/mnt/nuplan/dataset/processed/" # preprocess data using data_process.sh
+TRAIN_SET_LIST_PATH="/mnt/nuplan/projects/Diffusion-Planner/diffusion_planner_training.json"
+###################################
+
+$RUN_PYTHON_PATH -m torch.distributed.run --nnodes 1 --nproc-per-node 4 --standalone train_predictor.py \
+--train_set  $TRAIN_SET_PATH \
+--train_set_list  $TRAIN_SET_LIST_PATH \
