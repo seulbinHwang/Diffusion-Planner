428201988e799543cbae196b8ac90d49ece312c0
h.sb <jksg01019@gmail.com>
2025-08-24 13:07:40 +0900

    test

--- train_predictor.py
+++ train_predictor.py
@@ -159,7 +159,7 @@ def get_args():
     parser.add_argument('--batch_size',
                         type=int,
                         help='batch size (default: 2048)',
-                        default=1536)
+                        default=1024)
     parser.add_argument('--learning_rate',
                         type=float,
                         help='learning rate (default: 5e-4)',
