0e63e43693d1d86732cc67ccd63d9d64aa91464a
h-sb <h-sb@es.naverlabs.com>
2025-09-10 10:51:57 +0900

    doing

--- data_process_n_move_to_nubes_pnc.sh
+++ data_process_n_move_to_nubes_pnc.sh
@@ -50,7 +50,7 @@ echo "Step 3: Uploading processed data..."
 
 nubescli dir-upload "labs-mlops/ad/research/pnc/hsb/dataset/${TRAIN_SET_NAME}" \
                     "$TRAIN_SET_PATH" \
-                    -e -j 128
+                    -e -j 96
 
 nubescli upload labs-mlops/ad/research/pnc/hsb/dataset/${TRAIN_JSON_PATH}/diffusion_planner_training.json \
                     /media/user/E/projects/Diffusion-Planner/diffusion_planner_training.json
