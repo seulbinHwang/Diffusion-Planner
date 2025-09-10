4517b35b85a035c3d031a8c2abf0e22fdacf871f
h-sb <h-sb@es.naverlabs.com>
2025-09-10 10:10:12 +0900

    doing

--- data_process.py
+++ data_process.py
@@ -494,7 +494,7 @@ if __name__ == "__main__":
                         help='shuffle scenarios')
     parser.add_argument('--agent_num',
                         type=int,
-                        default=448,
+                        default=32,
                         help='number of agents')
     parser.add_argument('--static_objects_num',
                         type=int,
