206b8b7e8268305a026baa6d432c30842ce3837f
h-sb <h-sb@es.naverlabs.com>
2025-09-11 12:24:40 +0900

    doing

--- diffusion_planner/utils/normalizer.py
+++ diffusion_planner/utils/normalizer.py
@@ -14,8 +14,8 @@ class StateNormalizer:
     def from_json(cls, args):
         data = openjson(args.normalization_file_path)
         a  = [data["neighbor"]["mean"]]
-        print(
-            f"[StateNormalizer] a: shape: {data['neighbor']['mean']}")
+        # print(
+        #     f"[StateNormalizer] a: shape: {data['neighbor']['mean']}")
 
         mean = [[data["neighbor"]["mean"]]] * args.predicted_neighbor_num
         std =  [[data["neighbor"]["std"]]] * args.predicted_neighbor_num
@@ -67,7 +67,7 @@ class ObservationNormalizer:
         ndt = {}
         for k, v in data.items():
             if k not in ["ego", "neighbor"]:
-                print(f"[ObservationNormalizer] key: {k} is and value: {v['mean']}")
+                # print(f"[ObservationNormalizer] key: {k} is and value: {v['mean']}")
                 ndt[k] = {
                     "mean": torch.tensor(v["mean"], dtype=torch.float32),
                     "std": torch.tensor(v["std"], dtype=torch.float32)
