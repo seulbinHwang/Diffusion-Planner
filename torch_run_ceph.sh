19ad5232ad8281f45dfc3fef40f513e7d45d4fec
h.sb <jksg01019@gmail.com>
2025-09-22 11:58:25 +0900

    sdfadsf

--- torch_run_ceph.sh
+++ torch_run_ceph.sh
@@ -89,4 +89,4 @@ export TORCHELASTIC_ERROR_FILE="$LOG_DIR/torchelastic_error.json"
   --train_set_list "$TRAIN_SET_LIST_PATH" \
   --name "new-adaLN-weighted-loss-h-two" \
   --batch_size 4096
-#  --resume_local_path_model_path "/mnt/nuplan/projects/Diffusion-Planner/training_log/world-model-small/2025-09-11-05:32:07" \
+  --resume_local_path_model_path "/mnt/nuplan/projects/Diffusion-Planner/training_log/new-adaLN-weighted-loss-h-two/2025-09-21-13:25:45" \
