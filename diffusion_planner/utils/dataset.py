2d2fde4bf6d25c5dd0e525d52db7c48e1a99ba52
h.sb <jksg01019@gmail.com>
2025-10-26 00:21:51 +0900

    BIG DIFF

--- diffusion_planner/utils/dataset.py
+++ diffusion_planner/utils/dataset.py
@@ -32,7 +32,10 @@ class DiffusionPlannerData(Dataset):
         # (num_agents, future_len, 3) -> (predicted_neighbor_num, future_len, 3)
         near_future_gt_3_dim = data[
             "neighbor_future_gt_3_dim"][:self._predicted_neighbor_num]
-
+        # TODO: remove
+        neighbor_agents_future = data[
+            "neighbor_agents_future"][:self._predicted_neighbor_num]
+        ############
         lanes = data["lanes"]
         lanes_speed_limit = data["lanes_speed_limit"]
         lanes_has_speed_limit = data["lanes_has_speed_limit"]
@@ -62,7 +65,7 @@ class DiffusionPlannerData(Dataset):
             ###
             "near_future_gt_3_dim": neighbor_agents_future,  # 11 ###
             ### 유일하게 key 이름이 다름.
-            "planner_future_11_dim": ego_agent_future_11_dim,  # 12 ###
+            "planner_future_11_dim":  data["ego_agent_future_11_dim"],  # 12 ###
             "agent_route_lane_order": agent_route_lane_order,  # 13
         }
 
