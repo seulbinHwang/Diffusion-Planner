4a35d52e99dfb1720452c77a5f6b8250023883e8
h.sb <jksg01019@gmail.com>
2025-10-26 00:17:26 +0900

    BIG DIFF

--- diffusion_planner/utils/dataset.py
+++ diffusion_planner/utils/dataset.py
@@ -21,6 +21,7 @@ class DiffusionPlannerData(Dataset):
     def __len__(self):
         return len(self.data_list)
 
+
     def __getitem__(self, idx):
         data = opendata(os.path.join(self.data_dir, self.data_list[idx]))
 
@@ -49,8 +50,8 @@ class DiffusionPlannerData(Dataset):
             "ego_agent_past": data["ego_agent_past"],  # 0
             "ego_current_state": data["ego_current_state"],  # 1
             ###
-            "ego_future_gt_3_dim": data["ego_future_gt_3_dim"],  # 2
-            "neighbor_agents_past": neighbor_agents_past,  # 3
+            "ego_future_gt_3_dim": data["ego_agent_future"],  # 2
+            "neighbor_agents_past": neighbor_agents_past,  # 3 ###
             "lanes": lanes,  # 4
             "lanes_speed_limit": lanes_speed_limit,  # 5
             "lanes_has_speed_limit": lanes_has_speed_limit,  # 6
@@ -59,10 +60,57 @@ class DiffusionPlannerData(Dataset):
             "route_lanes_has_speed_limit": route_lanes_has_speed_limit,  # 9
             "static_objects": static_objects,  # 10
             ###
-            "near_future_gt_3_dim": near_future_gt_3_dim,  # 11
+            "near_future_gt_3_dim": neighbor_agents_future,  # 11 ###
             ### 유일하게 key 이름이 다름.
-            "planner_future_11_dim": ego_future_gt_11_dim,  # 12
+            "planner_future_11_dim": ego_agent_future_11_dim,  # 12 ###
             "agent_route_lane_order": agent_route_lane_order,  # 13
         }
 
         return tuple(data.values())
+    #
+    #
+    # def __getitem__(self, idx):
+    #     data = opendata(os.path.join(self.data_dir, self.data_list[idx]))
+    #
+    #     ego_future_gt_11_dim = data["ego_future_gt_11_dim"]
+    #
+    #     neighbor_agents_past = data["neighbor_agents_past"][:self.
+    #                                                         _past_neighbor_num]
+    #     # (num_agents, future_len, 3) -> (predicted_neighbor_num, future_len, 3)
+    #     near_future_gt_3_dim = data[
+    #         "neighbor_future_gt_3_dim"][:self._predicted_neighbor_num]
+    #
+    #     lanes = data["lanes"]
+    #     lanes_speed_limit = data["lanes_speed_limit"]
+    #     lanes_has_speed_limit = data["lanes_has_speed_limit"]
+    #
+    #     route_lanes = data["route_lanes"]
+    #     route_lanes_speed_limit = data["route_lanes_speed_limit"]
+    #     route_lanes_has_speed_limit = data["route_lanes_has_speed_limit"]
+    #     agent_route_lane_order = data["agent_route_lane_order"].astype(
+    #         "int64"
+    #     )[:self._predicted_neighbor_num]  # (predicted_neighbor_num, lane_num)
+    #
+    #     static_objects = data["static_objects"]
+    #
+    #     data = {
+    #         "ego_agent_past": data["ego_agent_past"],  # 0
+    #         "ego_current_state": data["ego_current_state"],  # 1
+    #         ###
+    #         "ego_future_gt_3_dim": data["ego_future_gt_3_dim"],  # 2
+    #         "neighbor_agents_past": neighbor_agents_past,  # 3
+    #         "lanes": lanes,  # 4
+    #         "lanes_speed_limit": lanes_speed_limit,  # 5
+    #         "lanes_has_speed_limit": lanes_has_speed_limit,  # 6
+    #         "route_lanes": route_lanes,  # 7
+    #         "route_lanes_speed_limit": route_lanes_speed_limit,  # 8
+    #         "route_lanes_has_speed_limit": route_lanes_has_speed_limit,  # 9
+    #         "static_objects": static_objects,  # 10
+    #         ###
+    #         "near_future_gt_3_dim": near_future_gt_3_dim,  # 11
+    #         ### 유일하게 key 이름이 다름.
+    #         "planner_future_11_dim": ego_future_gt_11_dim,  # 12
+    #         "agent_route_lane_order": agent_route_lane_order,  # 13
+    #     }
+    #
+    #     return tuple(data.values())
