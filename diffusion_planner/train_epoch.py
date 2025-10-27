385851a83d1a5551db1d70711b4d329bf4defbbf
h.sb <jksg01019@gmail.com>
2025-10-27 17:39:53 +0900

    BIG DIFF

--- diffusion_planner/train_epoch.py
+++ diffusion_planner/train_epoch.py
@@ -109,7 +109,7 @@ def train_epoch(data_loader,
             # loss backward
             loss["loss"].backward()
 
-            nn.utils.clip_grad_norm_(model.parameters(), 15)
+            # nn.utils.clip_grad_norm_(model.parameters(), 20)
             scheduler.step()
             optimizer.step()
             # === WD warmdown: lr 비례로 그룹별 WD 갱신 ===
