be2f6658af6594e82ea482b338587403e6b38e1e
h.sb <jksg01019@gmail.com>
2025-11-08 23:24:50 +0900

    START

--- torch_run_ceph_2.sh
+++ torch_run_ceph_2.sh
@@ -95,7 +95,7 @@ export TORCHELASTIC_ERROR_FILE="$LOG_DIR/torchelastic_error.json"
   --train_set "$TRAIN_SET_PATH"/ \
   --train_set_list "$TRAIN_SET_LIST_PATH" \
   --name "use_pram_false_2_gpu_B" \
-  --batch_size 2048 \
+  --batch_size 1536 \
   --seed 7777 \
   --sampler_epoch_offset 1000 \
   --use_pram false \
