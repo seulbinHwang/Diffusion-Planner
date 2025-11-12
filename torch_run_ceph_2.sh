3efa2905cafa6d3bd2894cca5a20e245fa2b1f07
h-sb <h-sb@es.naverlabs.com>
2025-11-12 19:33:56 +0900

    실험 A: savgol + control to state integration

--- torch_run_ceph_2.sh
+++ torch_run_ceph_2.sh
@@ -94,11 +94,11 @@ export TORCHELASTIC_ERROR_FILE="$LOG_DIR/torchelastic_error.json"
   --port 23002 \
   --train_set "$TRAIN_SET_PATH"/ \
   --train_set_list "$TRAIN_SET_LIST_PATH" \
-  --name "use_pram_false_2_gpu_B" \
+  --name "use_feasible_train_true_2_gpu_B" \
   --batch_size 1280 \
   --seed 7777 \
   --sampler_epoch_offset 1000 \
   --use_feasible true \
-  --use_feasible_train false \
+  --use_feasible_train true \
   "$@"
 #  --resume_local_path_model_path "/mnt/nuplan/projects/Diffusion-Planner/training_log/new-adaLN-weighted-loss-h-two/2025-09-21-13:25:45" \
