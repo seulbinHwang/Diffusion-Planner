c2430219b74e78ae27cbb6b393aed3558650abe3
h.sb <jksg01019@gmail.com>
2025-11-19 22:17:04 +0900

    doing

--- torch_run_ceph_2.sh
+++ torch_run_ceph_2.sh
@@ -94,7 +94,7 @@ export TORCHELASTIC_ERROR_FILE="$LOG_DIR/torchelastic_error.json"
   --port 23002 \
   --train_set "$TRAIN_SET_PATH"/ \
   --train_set_list "$TRAIN_SET_LIST_PATH" \
-  --name "use_feasible_train_wo_past_2_gpu_B" \
+  --name "feasible_0.4_time_gpu_2_exp_B" \
   --batch_size 1792 \
   --seed 7777 \
   --sampler_epoch_offset 1000 \
