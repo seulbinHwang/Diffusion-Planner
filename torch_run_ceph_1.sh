06f74833b9969b0c9b52afc1879acc493122821c
h.sb <jksg01019@gmail.com>
2025-11-19 18:32:40 +0900

    doing

--- torch_run_ceph_1.sh
+++ torch_run_ceph_1.sh
@@ -96,7 +96,7 @@ export TORCHELASTIC_ERROR_FILE="$LOG_DIR/torchelastic_error.json"
   --train_set "$TRAIN_SET_PATH"/ \
   --train_set_list "$TRAIN_SET_LIST_PATH" \
   --name "feasible_full_time_gpu_2_exp_A" \
-  --batch_size 2048 \
+  --batch_size 1792 \
   --profile_feasible false \
   "$@"
 #  --resume_local_path_model_path "/mnt/nuplan/projects/Diffusion-Planner/training_log/new-adaLN-weighted-loss-h-two/2025-09-21-13:25:45" \
