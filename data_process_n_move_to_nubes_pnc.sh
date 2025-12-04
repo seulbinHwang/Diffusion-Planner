9b97a82258ea8250cdad1f82807c1920b7eae839
h-sb <h-sb@es.naverlabs.com>
2025-12-04 17:36:05 +0900

    doing

--- data_process_n_move_to_nubes_pnc.sh
+++ data_process_n_move_to_nubes_pnc.sh
@@ -30,7 +30,7 @@ TRAIN_JSON_PATH="${TRAIN_SET_NAME}_json"
 # Run the data processing script
 # This is the command from data_process_pnc.sh
 CUDA_VISIBLE_DEVICES= NVIDIA_VISIBLE_DEVICES= PYTORCH_ENABLE_MPS_FALLBACK=0 \
-taskset -c 0-3 \
+taskset -c 0-127 \
 python data_process.py \
   --data_path "$NUPLAN_DATA_PATH" \
   --map_path "$NUPLAN_MAP_PATH" \
