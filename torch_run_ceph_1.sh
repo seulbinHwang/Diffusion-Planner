b4e2a687fb729de16533bd6c369762879ea752b1
h-sb <h-sb@es.naverlabs.com>
2025-12-06 14:46:41 +0900

    doing

--- torch_run_ceph_1.sh
+++ torch_run_ceph_1.sh
@@ -112,7 +112,7 @@ export TORCHELASTIC_ERROR_FILE="$LOG_DIR/torchelastic_error.json"
   --train_set "$TRAIN_SET_PATH"/ \
   --train_set_list "$TRAIN_SET_LIST_PATH" \
   --name "feasible_full_time_use_vel_gpu_2_exp_A" \
-  --batch_size 1792 \
+  --batch_size 1024 \
   --profile_feasible false \
   --use_vel_input false \
   --use_deepspeed true \
