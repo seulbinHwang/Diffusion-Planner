728f7e6857a5c89e663ce5f154eec461a9877e8c
h-sb <h-sb@es.naverlabs.com>
2025-12-10 20:45:57 +0900

    doing

--- diffusion_planner/train_epoch.py
+++ diffusion_planner/train_epoch.py
@@ -664,10 +664,6 @@ def train_epoch(
         batch_num_in_epoch=len(data_loader),
     )
     with tqdm(data_loader, desc="Training", unit="batch") as data_epoch:
-        for batch in data_epoch:
-            if args._global_update_step >= 1:
-                print(f"Global step: {args._global_update_step}")
-                break
             # 1) device 이동 + 상한 클리핑 + 정답 분리
             inputs, outputs = _prepare_batch_for_device(
                 batch,
