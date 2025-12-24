c9d1dfe557af5cca723699261300e1633ab3213c
h-sb <h-sb@es.naverlabs.com>
2025-12-24 14:53:36 +0900

    doing

--- diffusion_planner/utils/dataset.py
+++ diffusion_planner/utils/dataset.py
@@ -295,6 +295,8 @@ class DiffusionPlannerData(Dataset):
                 out_key = npz_key_to_new_key.get(npz_key, npz_key)
                 sample[out_key] = value
             if agent_route_lane_order_agent_num is not None:
+                print("agent_route_lane_order_agent_num:", agent_route_lane_order_agent_num,
+                        "neighbor_agents_past_agent_num:", neighbor_agents_past_agent_num)
                 assert agent_route_lane_order_agent_num == neighbor_agents_past_agent_num, (
                     f"agent_route_lane_order_agent_num ({agent_route_lane_order_agent_num}) != "
                     f"neighbor_agents_past_agent_num ({neighbor_agents_past_agent_num}) "
