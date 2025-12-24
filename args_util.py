bb497037abc72148f29861d387355f36c3a8951b
h-sb <h-sb@es.naverlabs.com>
2025-12-24 23:41:50 +0900

    doing

--- args_util.py
+++ args_util.py
@@ -222,7 +222,7 @@ def get_args():
     parser.add_argument('--do_ego_predict', default=True, type=boolean)
     parser.add_argument('--use_ego_data_augment', default=False, type=boolean)
     parser.add_argument('--use_npc_data_augment', default=False, type=boolean)
-    parser.add_argument('--num_workers', default=4, type=int)
+    parser.add_argument('--num_workers', default=3, type=int)
     parser.add_argument('--pin_mem', default=True, type=boolean)
     parser.add_argument('--set_coord_as_center', default=True, type=boolean)
     parser.add_argument('--use_feasible', default=True, type=boolean)
@@ -359,7 +359,7 @@ def get_args():
     parser.add_argument('--prefetch_factor',
                         type=int,
                         help='number of warm up',
-                        default=4)
+                        default=3)
     parser.add_argument('--encoder_drop_path_rate',
                         type=float,
                         help='encoder drop out rate',
