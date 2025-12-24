b6da1da5f37185570675f32c68e4ec0aa8df3d3a
h-sb <h-sb@es.naverlabs.com>
2025-12-24 23:30:10 +0900

    doing

--- torch_run_ceph.sh
+++ torch_run_ceph.sh
@@ -51,7 +51,7 @@ TRAIN_SET_LIST_PATH="/mnt/nuplan/projects/Diffusion-Planner/diffusion_planner_tr
   --train_set "$TRAIN_SET_PATH"/ \
   --train_set_list "$TRAIN_SET_LIST_PATH" \
   --name "nuplan_womd" \
-  --batch_size 2048 \
+  --batch_size 1536 \
   --use_deepspeed True \
   --use_ego_plan False
   "$@"
