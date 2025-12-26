924bdf4e1b9e26a6d188149a2fa74d6074c42077
h-sb <h-sb@es.naverlabs.com>
2025-12-26 19:39:10 +0900

    doing

--- args_util.py
+++ args_util.py
@@ -285,7 +285,7 @@ def get_args():
     parser.add_argument('--save_utd',
                         type=int,
                         help='save frequency',
-                        default=20)
+                        default=10)
     parser.add_argument('--batch_size',
                         type=int,
                         help='batch size (default: 2048)',
