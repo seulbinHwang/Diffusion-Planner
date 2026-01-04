1ebfbf2f98ece3567b1087b26ccc0e80508e3f63
h-sb <h-sb@es.naverlabs.com>
2026-01-04 23:33:18 +0900

    doing

--- validation_pnc.sh
+++ validation_pnc.sh
@@ -179,6 +179,6 @@ export TORCHELASTIC_ERROR_FILE="$LOG_DIR/torchelastic_error.json"
   --run_count "$RUN_COUNT" \
   --total_save_image_trial_num 1 \
   --rollout_time_chunk_size 5 \
-  --use_data_percent 1
+  --use_data_percent 2
 
 #  --resume_local_path_model_path "/mnt/nuplan/projects/Diffusion-Planner/training_log/new-adaLN-weighted-loss-h-two/2025-09-21-13:25:45" \
