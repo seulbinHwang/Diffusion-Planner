c62f412539b8d1aea528a68afac7bd097a519579
h-sb <h-sb@es.naverlabs.com>
2026-01-04 15:02:48 +0900

    doing

--- validation_pnc.sh
+++ validation_pnc.sh
@@ -135,7 +135,7 @@ export CUDA_DEVICE_MAX_CONNECTIONS=32
 # - env에 이미 값이 있으면 그 값을 그대로 사용
 # - 없으면 기본값을 넣어서 CPU 스레드/프로세스 경쟁을 줄임
 # ----------------------------
-export DP_WOSAC_TF_THREADS="${DP_WOSAC_TF_THREADS:-8}"
+export DP_WOSAC_TF_THREADS="${DP_WOSAC_TF_THREADS:-1}"
 export DP_WOSAC_CPU_FRACTION="${DP_WOSAC_CPU_FRACTION:-0.75}"
 
 printf "[ENV] %-28s %s\n" "DP_WOSAC_TF_THREADS:"   "${DP_WOSAC_TF_THREADS-<unset>}"
@@ -175,7 +175,7 @@ export TORCHELASTIC_ERROR_FILE="$LOG_DIR/torchelastic_error.json"
   --save_image False \
   --save_video False \
   --finish_when_no_updated_pt True \
-  --validate_scenario_rollouts True \
+  --validate_scenario_rollouts False \
   --run_count "$RUN_COUNT" \
   --total_save_image_trial_num 1 \
   --rollout_time_chunk_size 5
