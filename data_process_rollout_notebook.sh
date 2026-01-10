f96982bd3a8c867906c9486b97d4357c71e5e4b5
h-sb <h-sb@es.naverlabs.com>
2026-01-10 12:02:07 +0900

    doing

--- data_process_rollout_notebook.sh
+++ data_process_rollout_notebook.sh
@@ -181,6 +181,7 @@ export TORCHELASTIC_ERROR_FILE="$LOG_DIR/torchelastic_error.json"
   --total_save_image_trial_num 30000 \
   --rollout_time_chunk_size 5 \
   --fine_tune_temperature 0.8 \
-  --use_recovery True
+  --use_recovery False \
+  --select_jointly False
 
 #  --resume_local_path_model_path "/mnt/nuplan/projects/Diffusion-Planner/training_log/new-adaLN-weighted-loss-h-two/2025-09-21-13:25:45" \
