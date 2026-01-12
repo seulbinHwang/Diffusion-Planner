bacf0bc692353dced30c64ec12f78cab3150a7e3
h-sb <h-sb@es.naverlabs.com>
2026-01-12 23:29:33 +0900

    doing

--- validation_pnc.sh
+++ validation_pnc.sh
@@ -165,8 +165,8 @@ export TORCHELASTIC_ERROR_FILE="$LOG_DIR/torchelastic_error.json"
   --eval_set_list "$TRAIN_SET_LIST_PATH" \
   --resume_wandb_model_name latest \
   --resume_model_only True \
-  --load_name "nuplan_womd" \
-  --name "nuplan_womd" \
+  --load_name "nuplan_womd_fine_tuning1" \
+  --name "nuplan_womd_fine_tuning1" \
   --eval_method "validation" \
   --batch_size 256 \
   --use_deepspeed True \
