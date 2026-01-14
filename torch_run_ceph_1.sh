8850474caf5ffc2d941954cb3cb36d29382af01d
h-sb <h-sb@es.naverlabs.com>
2026-01-14 12:48:15 +0900

    doing

--- torch_run_ceph_1.sh
+++ torch_run_ceph_1.sh
@@ -54,4 +54,4 @@ STAGE1_CFG="configs/fine_tuning1.yaml"
   --train_set "$TRAIN_SET_PATH"/ \
   --train_set_list "$TRAIN_SET_LIST_PATH" \
   --stage_config_path "${STAGE1_CFG}" \
-  --batch_size 512
+  --batch_size 576
