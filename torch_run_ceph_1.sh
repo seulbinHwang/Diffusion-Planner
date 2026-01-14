1d98d7cd593efd9100129e1002cff647393461ee
h-sb <h-sb@es.naverlabs.com>
2026-01-14 11:40:12 +0900

    doing

--- torch_run_ceph_1.sh
+++ torch_run_ceph_1.sh
@@ -19,7 +19,7 @@ export CUDA_DEVICE_MAX_CONNECTIONS=32
 # CPU에서 돌아가는 연산(전처리, 일부 텐서 연산, BLAS 등)의 스레드 수를 컨트롤해서, GPU 학습 중 CPU 과도한 스레드 난립 방지
 printf "[ENV] %-28s %s\n" "OMP_NUM_THREADS:"            "${OMP_NUM_THREADS-<unset>}"
 printf "[ENV] %-28s %s\n" "CUDA_DEVICE_MAX_CONNECTIONS:" "${CUDA_DEVICE_MAX_CONNECTIONS-<unset>}"
-DEBUG_LOG=0  # 1: 상세 디버그, 0: 일반 학습
+DEBUG_LOG=1  # 1: 상세 디버그, 0: 일반 학습
 
 if (( DEBUG_LOG )); then
   export NCCL_DEBUG=INFO
