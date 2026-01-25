6ab374935555f878386df07234114ec87ca2cf16
h.sb <jksg01019@gmail.com>
2026-01-25 19:59:37 +0900

    doing

--- torch_run_ceph.sh
+++ torch_run_ceph.sh
@@ -50,6 +50,10 @@ TRAIN_SET_LIST_PATH="/mnt/nuplan/projects/Diffusion-Planner/diffusion_planner_tr
  train_predictor.py \
   --train_set "$TRAIN_SET_PATH"/ \
   --train_set_list "$TRAIN_SET_LIST_PATH" \
-  --name "wosac_test_full_7e4" \
+  --name "wosac_TTT_full_7e4" \
   --batch_size 1248 \
-  --learning_rate 7e-4
+  --learning_rate 7e-4 \
+  --use_feasible_dl True \
+  --feasible_grad_to_dit True \
+  --use_direct_loss True
+
