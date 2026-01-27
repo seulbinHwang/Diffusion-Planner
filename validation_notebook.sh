651094ffbd7f3b9501289ffa37f16d9a93a46a21
h-sb <h-sb@es.naverlabs.com>
2026-01-27 13:47:21 +0900

    doing

--- validation_notebook.sh
+++ validation_notebook.sh
@@ -194,7 +194,7 @@ taskset -c "${CPUSET}" \
   --load_name "wosac_test_full" \
   --name "wosac_test_full" \
   --eval_method "validation" \
-  --batch_size 1024 \
+  --batch_size 3 \
   --use_deepspeed True \
   --wosac_sub_is_active False \
   --wosac_metric_is_active False \
