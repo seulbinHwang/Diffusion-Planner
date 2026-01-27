a827d4ad58420982df74ff76da1a8e4ec16ec0b3
h-sb <h-sb@es.naverlabs.com>
2026-01-27 13:33:47 +0900

    doing

--- validation_pnc.sh
+++ validation_pnc.sh
@@ -204,7 +204,7 @@ taskset -c "${CPUSET}" \
   --wosac_metric_is_active True \
   --save_image False \
   --save_video False \
-  --finish_when_no_updated_pt True \
+  --finish_when_no_updated_pt False \
   --validate_scenario_rollouts False \
   --run_count "$RUN_COUNT" \
   --total_save_image_trial_num 1 \
