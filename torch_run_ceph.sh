c745302ff3f3b27bd15cc747e642b94b7c8c60e3
h-sb <h-sb@es.naverlabs.com>
2026-01-28 00:53:38 +0900

    doing

--- torch_run_ceph.sh
+++ torch_run_ceph.sh
@@ -52,5 +52,5 @@ TRAIN_SET_LIST_PATH="/mnt/nuplan/projects/Diffusion-Planner/diffusion_planner_tr
   --train_set_list "$TRAIN_SET_LIST_PATH" \
   --name "wosac_TTT_full_7e4" \
   --batch_size 1248 \
-  --learning_rate 7e-4 \
+  --learning_rate 7e-4
 
