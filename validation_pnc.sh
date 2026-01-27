f799b683898a30d08609c3dc30125463e22776f4
h-sb <h-sb@es.naverlabs.com>
2026-01-27 13:48:09 +0900

    doing

--- validation_pnc.sh
+++ validation_pnc.sh
@@ -198,7 +198,7 @@ taskset -c "${CPUSET}" \
   --load_name "wosac_test_full" \
   --name "wosac_test_full" \
   --eval_method "validation" \
-  --batch_size 1024 \
+  --batch_size 512 \
   --use_deepspeed True \
   --wosac_sub_is_active False \
   --wosac_metric_is_active True \
