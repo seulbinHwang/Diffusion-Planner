ef910796174d5dc58efdb2c414072d9553bba7c0
h-sb <h-sb@es.naverlabs.com>
2026-02-02 11:49:30 +0900

    doing

--- torch_run_pnc.sh
+++ torch_run_pnc.sh
@@ -2,9 +2,9 @@
 set -Eeuo pipefail
 export WANDB_DEBUG=1   # ← 여기 추가
 export PYTHONUNBUFFERED=1
-export CUDA_HOME="$CONDA_PREFIX"
-export PATH="$CUDA_HOME/bin:$PATH"
-export LD_LIBRARY_PATH="$CUDA_HOME/lib64:$LD_LIBRARY_PATH"
+#export CUDA_HOME="$CONDA_PREFIX"
+#export PATH="$CUDA_HOME/bin:$PATH"
+#export LD_LIBRARY_PATH="$CUDA_HOME/lib64:$LD_LIBRARY_PATH"
 RUN_ID=$(date +%Y%m%d-%H%M%S)
 LOG_DIR=/mnt/nuplan/logs/$RUN_ID
 mkdir -p "$LOG_DIR"
