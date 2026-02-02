986ac85db1afe2c0bf52ad83f9cd4a86386efb42
h-sb <h-sb@es.naverlabs.com>
2026-02-02 22:55:13 +0900

    doing

--- statistics_pnc.sh
+++ statistics_pnc.sh
@@ -213,6 +213,6 @@ taskset -c "${CPUSET}" \
   --use_data_percent 100 \
   --use_amortized_diffusion True \
   --do_data_statistics True \
-  --histogram_output_dir "histogram_all2"
+  --histogram_output_dir "histogram_all_0202"
 
 #  --resume_local_path_model_path "/mnt/nuplan/projects/Diffusion-Planner/training_log/new-adaLN-weighted-loss-h-two/2025-09-21-13:25:45" \
