4b67d0c00b9bf907fae5acabfe8c288168ce2a19
h-sb <h-sb@es.naverlabs.com>
2026-02-04 14:20:53 +0900

    doing

--- torch_run_ceph.sh
+++ torch_run_ceph.sh
@@ -45,12 +45,14 @@ TRAIN_SET_LIST_PATH="/mnt/nuplan/projects/Diffusion-Planner/diffusion_planner_tr
 
 
 "$RUN_PYTHON_PATH" -u -X faulthandler -m torch.distributed.run \
---nnodes 1 --nproc-per-node 6 --standalone --log_dir "$LOG_DIR" \
+--nnodes 1 --nproc-per-node 1 --standalone --log_dir "$LOG_DIR" \
 --redirects 3 --tee "$TEE" \
  train_predictor.py \
   --train_set "$TRAIN_SET_PATH"/ \
   --train_set_list "$TRAIN_SET_LIST_PATH" \
-  --name "wosac_final_0128" \
-  --batch_size 1248 \
-  --learning_rate 7e-4
+  --name "wosac_final_0204" \
+  --batch_size 208 \
+  --learning_rate 7e-4 \
+  --profile_feasible True \
+  --feasible_stride_dt 0.1
 
