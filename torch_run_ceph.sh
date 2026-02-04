49010b5df9f469781b49c806a0c26873121202bd
h-sb <h-sb@es.naverlabs.com>
2026-02-04 15:48:35 +0900

    doing

--- torch_run_ceph.sh
+++ torch_run_ceph.sh
@@ -45,8 +45,8 @@ TRAIN_SET_LIST_PATH="/mnt/nuplan/projects/Diffusion-Planner/diffusion_planner_tr
 
 
 "$RUN_PYTHON_PATH" -u -X faulthandler -m torch.distributed.run \
---nnodes 1 --nproc-per-node 1 --standalone --log_dir "$LOG_DIR" \
---redirects 6 --tee "$TEE" \
+--nnodes 1 --nproc-per-node 6 --standalone --log_dir "$LOG_DIR" \
+--redirects 3 --tee "$TEE" \
  train_predictor.py \
   --train_set "$TRAIN_SET_PATH"/ \
   --train_set_list "$TRAIN_SET_LIST_PATH" \
