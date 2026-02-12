9bf36aae7bb24f92e19194a85a1a522c5e5dcf8d
h-sb <h-sb@es.naverlabs.com>
2026-02-12 20:59:17 +0900

    doing

--- torch_run_ceph.sh
+++ torch_run_ceph.sh
@@ -90,7 +90,7 @@ if (( USE_LOCAL_SHARDS )); then
       --seed 3407 \
       --predicted_neighbor_num 448 \
       --use_agent_route_lane_order false \
-      --force_rebuild true \
+      --force_rebuild false \
       --progress_interval_min 5 \
       --build_workers 24
 
