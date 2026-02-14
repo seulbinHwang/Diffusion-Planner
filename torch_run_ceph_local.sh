da513172e06c77a02509c0d21459f050b91c6938
h-sb <h-sb@es.naverlabs.com>
2026-02-14 10:13:49 +0900

    doing

--- torch_run_ceph_local.sh
+++ torch_run_ceph_local.sh
@@ -114,4 +114,6 @@ FORCE_REBUILD=0
     --use_feasible False \
     --use_feasible_dl False \
     --use_feasible_filter False \
-    --feasible_stride_dt 0.1
+    --feasible_stride_dt 0.1 \
+    --num_workers 3 \
+    --prefetch_factor 8
