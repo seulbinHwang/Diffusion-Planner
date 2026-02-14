bfc5a39c519bc54e6ce7718599614dbab9c1cbef
h-sb <h-sb@es.naverlabs.com>
2026-02-15 01:20:42 +0900

    doing

--- torch_run_ceph_local.sh
+++ torch_run_ceph_local.sh
@@ -121,7 +121,10 @@ FORCE_REBUILD=0
     --num_workers 3 \
     --prefetch_factor 8 \
     --pose_based False \
-    --p_sat 0. \
-    --w_int_max 1.0 \
-    --w_const 0.1
+    --p_sat 0.30 \
+    --w_dir 1.0 \
+    --w_int_min 0.05 \
+    --w_int_max 3.0 \
+    --w_const 0.10
+
 
