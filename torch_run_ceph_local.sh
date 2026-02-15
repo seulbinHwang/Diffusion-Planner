6b76ee6c29ca64760facc9d70e2d4905b7ed4285
h-sb <h-sb@es.naverlabs.com>
2026-02-15 16:26:35 +0900

    doing

--- torch_run_ceph_local.sh
+++ torch_run_ceph_local.sh
@@ -110,9 +110,9 @@ export DP_ENABLE_CPU_MONITOR=0
   train_predictor.py \
     --train_set "$LOCAL_TRAIN_SET_PATH"/ \
     --train_set_list "$LOCAL_TRAIN_SET_LIST_PATH" \
-    --name "38_control_only7e-4" \
+    --name "38_control_only5e-3" \
     --batch_size 1536 \
-    --learning_rate 1e-3 \
+    --learning_rate 5e-3 \
     --min_learning_rate 1e-6 \
     --profile_feasible False \
     --use_feasible False \
