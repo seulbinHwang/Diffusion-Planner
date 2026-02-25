65eb6c195da02e43828d46bceb177cbdc5263586
h-sb <h-sb@es.naverlabs.com>
2026-02-26 00:11:23 +0900

    doing

--- diffusion_planner/model/module/feasible.py
+++ diffusion_planner/model/module/feasible.py
@@ -1202,21 +1202,21 @@ class FeasibleProjector(nn.Module):
             eta=hp.eta_speed,
             eps=hp.eps,
         )
+        # omega_after = omega_raw
 
-        # vx_after, vy_after, omega_after = self._apply_S2_accel_alpha_limits_ste_batch(
-        #     vx_b=vx_after,
-        #     vy_b=vy_after,
-        #     omega=omega_raw,
-        #     a_max=key_to_limit_bp["a_max"],
-        #     alpha_max=key_to_limit_bp["alpha_max"],
-        #     dt=float(hp.dt),
-        #     eta=float(hp.eta_inc),
-        #     eps=float(hp.eps),
-        #     is_nonholonomic=key_to_limit_bp["is_nonholonomic"],  # ✅ 추가
-        #     target_current_control=target_current_control,
-        #     target_current_control_valid=target_current_control_valid,
-        # )
-        omega_after = omega_raw
+        vx_after, vy_after, omega_after = self._apply_S2_accel_alpha_limits_ste_batch(
+            vx_b=vx_after,
+            vy_b=vy_after,
+            omega=omega_raw,
+            a_max=key_to_limit_bp["a_max"],
+            alpha_max=key_to_limit_bp["alpha_max"],
+            dt=float(hp.dt),
+            eta=float(hp.eta_inc),
+            eps=float(hp.eps),
+            is_nonholonomic=key_to_limit_bp["is_nonholonomic"],  # ✅ 추가
+            target_current_control=target_current_control,
+            target_current_control_valid=target_current_control_valid,
+        )
 
         omega_after = self._apply_S3_omega_clip_ste(
             vx_b=vx_after,
