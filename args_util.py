1e216982dff8634211789efc968c17d661dcaaa3
h-sb <h-sb@es.naverlabs.com>
2026-02-28 19:14:40 +0900

    doing

--- args_util.py
+++ args_util.py
@@ -370,7 +370,7 @@ def get_args():
     parser.add_argument('--save_utd',
                         type=int,
                         help='save frequency',
-                        default=7)
+                        default=5)
     parser.add_argument('--xy_yaw_metric_interval_steps',
                         type=int,
                         help='save frequency',
