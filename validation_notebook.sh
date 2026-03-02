5b53081030ad74eb96554a8ecf825e5c5124e785
h-sb <h-sb@es.naverlabs.com>
2026-03-02 15:54:03 +0900

    doing

--- validation_notebook.sh
+++ validation_notebook.sh
@@ -191,15 +191,15 @@ taskset -c "${CPUSET}" \
   --eval_set_list "$EVAL_SET_LIST_PATH" \
   --resume_wandb_model_name latest \
   --resume_model_only True \
-  --load_name "w_integ_loss_thres_1_p_sat_0.25_ft_1" \
-  --name "w_integ_loss_thres_1_p_sat_0.25_ft_1" \
+  --load_name "w_integ_loss_thres_1_p_sat_0.25_sft" \
+  --name "w_integ_loss_thres_1_p_sat_0.25_sft" \
   --eval_method "validation" \
   --batch_size 1 \
   --pose_based False \
   --profile_feasible False \
   --use_feasible True \
   --use_feasible_dl False \
-  --use_feasible_filter True \
+  --use_feasible_filter False \
   --feasible_grad_to_dit False \
   --use_deepspeed True \
   --wosac_sub_is_active False \
@@ -212,6 +212,8 @@ taskset -c "${CPUSET}" \
   --total_save_image_trial_num 200 \
   --rollout_number 1 \
   --do_data_statistics False \
-  --use_amortized_diffusion False
+  --use_amortized_diffusion False \
+  --use_npc_data_augment False \
+  --use_body_vel True
 
 #  --resume_local_path_model_path "/mnt/nuplan/projects/Diffusion-Planner/training_log/new-adaLN-weighted-loss-h-two/2025-09-21-13:25:45" \
