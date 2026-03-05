161dd8de1b15b704a8523928efbc3227d5436557
h-sb <h-sb@es.naverlabs.com>
2026-03-05 23:03:03 +0900

    doing

--- torch_run_ceph_local_load.sh
+++ torch_run_ceph_local_load.sh
@@ -112,8 +112,8 @@ FORCE_REBUILD=0
   train_predictor.py \
     --train_set "$LOCAL_TRAIN_SET_PATH"/ \
     --train_set_list "$LOCAL_TRAIN_SET_LIST_PATH" \
-    --name "use_npc_data_augment_true_use_body_vel_false" \
-    --load_name "use_npc_data_augment_true_use_body_vel_false_test" \
+    --name "use_npc_data_augment_true_use_body_vel_false_test" \
+    --load_name "use_npc_data_augment_true_use_body_vel_false" \
     --resume_wandb_model_name latest \
     --resume_model_only False \
     --batch_size 1536 \
