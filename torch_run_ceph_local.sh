faf51de71ee735cfe33b3628e3483b091ae41f34
h-sb <h-sb@es.naverlabs.com>
2026-03-07 16:01:08 +0900

    doing

--- torch_run_ceph_local.sh
+++ torch_run_ceph_local.sh
@@ -112,7 +112,7 @@ FORCE_REBUILD=0
   train_predictor.py \
     --train_set "$LOCAL_TRAIN_SET_PATH"/ \
     --train_set_list "$LOCAL_TRAIN_SET_LIST_PATH" \
-    --name "use_npc_data_augment_true_use_body_vel_false_new_norm" \
+    --name "use_npc_data_augment_true_use_body_vel_true_feasible" \
     --batch_size 1536 \
     --train_epochs 400 \
     --learning_rate 1e-3 \
